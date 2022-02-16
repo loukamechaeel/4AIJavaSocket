82d44227ca2f967baea218b09abe9e9aebd205e7
Louka Mechaeel <louka.mechaeel@osdbsesto>
2022-02-16 09:34:26 +0100

    java

--- JavaApplication2/src/javaapplication2/ServerSocket.java
+++ JavaApplication2/src/javaapplication2/ServerSocket.java
@@ -9,13 +9,15 @@ package javaapplication2;
  *
  * @author louka.mechaeel
  */
-public class JavaApplication2 {
+public class ServerSocket {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+      
+        
     }
     
 }
